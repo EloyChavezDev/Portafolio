e5bc14e1732f13d7afbec6ca5ec762bb1433db32
EloyChavezDev <laneloychavez@gmail.com>
2023-07-30 14:58:22 -0600

    Update landing
    Actualización de datos e información a la portada del sitio

--- app/page.tsx
+++ app/page.tsx
@@ -35,26 +35,25 @@ export default function Home() {
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					Hi, my name is Andreas, I'm building serverless and open source
-					solutions at{" "}
+					Hi,my name is Eloy. I learn and teach technological solutions in my day-to-day life on{" "}
 					<Link
 						target="_blank"
-						href="https://upstash.com"
+						href="https://www.youtube.com/@eloychavezdev"
 						className="underline duration-500 hover:text-zinc-300"
 					>
-						Upstash
+						YouTube,
 					</Link>
 
 					<br />
-					and working on{" "}
+					and I work as a systems assistant at{" "}
 					<Link
 						target="_blank"
 						href="https://unkey.dev"
 						className="underline duration-500 hover:text-zinc-300"
 					>
-						unkey.dev
+						UCA University ()
 					</Link>{" "}
-					at night.
+					.
 				</h2>
 			</div>
 		</div>
